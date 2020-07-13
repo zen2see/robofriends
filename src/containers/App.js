da4c55286f93d7ce9be0321eec02f0c79982aced
z2s <zen2see@gmail.com>
2020-07-12 22:59:20 -0400

    Updated App.js fetch link to https, tc for Loading message

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   }    
 
   componentDidMount() {
-    fetch('http://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ robots: users }));
   }
@@ -29,7 +29,7 @@ class App extends Component {
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase()); 
     })
-    return !robots.length ? <h1>Loading</h1> :
+    return !robots.length ? <h1 className="tc">Loading...</h1> :
     (
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
