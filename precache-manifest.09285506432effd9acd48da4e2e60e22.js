c56fcdf1dad1ab815a2b850ca14d1442bfa8e452
z2s <zen2see@gmail.com>
2020-07-12 22:44:30 -0400

    Updates

---
+++ precache-manifest.09285506432effd9acd48da4e2e60e22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a80b0663c282169ef2ab5e87131c8dd",
+    "url": "/zen2see/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/zen2see/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "be07c44b38b7d537f6ea",
+    "url": "/zen2see/robofriends/static/css/main.61d32c02.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/zen2see/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zen2see/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be07c44b38b7d537f6ea",
+    "url": "/zen2see/robofriends/static/js/main.4f9da4d9.chunk.js"
+  },
+  {
+    "revision": "e3f6c77a9c7d2d75bedc",
+    "url": "/zen2see/robofriends/static/js/runtime-main.f9ff45ee.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/zen2see/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
