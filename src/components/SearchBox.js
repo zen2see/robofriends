a3cda28651ba316629ff69cb29feceab45e7083f
z2s <zen2see@gmail.com>
2020-07-20 07:16:39 -0400

    Added aria-label='Search Robots' instead of label for

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,13 +3,13 @@ import React from 'react';
 const SearchBox = ({searchfield, searchChange}) => {
   return (
     <div className='pa2'>
-      <input id='searchRobots'
+      <input 
+        aria-label='Search Robots'
         className='pa5 ba b--green bg-lightest-blue'
         type='search' 
         placeholder='search robots'
         onChange={searchChange}
       />
-      <label for='searchRobots'>Search Robots</label>
     </div>
   );
 }
