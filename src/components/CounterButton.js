80f573672bbf2c5df0fe8f9254c766755970b6a4
z2s <zen2see@gmail.com>
2020-07-20 17:46:55 -0400

    Added serveral test

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -22,9 +22,11 @@ class CounterButton extends Component {
   }
 
   render() {
-    console.log('CounterButton')
     return (
-      <button color={this.props.color} onClick={this.updateCount}>
+      <button 
+        id='counter'
+        color={this.props.color} 
+        onClick={this.updateCount}>
         Count: {this.state.count}
       </button>
     );
