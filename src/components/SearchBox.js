a8a5ede20ce3fb0e4a25338e177d95b35cbde5c4
z2s <zen2see@gmail.com>
2020-07-20 07:12:44 -0400

    Added an id and label for... for SearchBox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,12 +3,13 @@ import React from 'react';
 const SearchBox = ({searchfield, searchChange}) => {
   return (
     <div className='pa2'>
-      <input
+      <input id='searchRobots'
         className='pa5 ba b--green bg-lightest-blue'
         type='search' 
         placeholder='search robots'
         onChange={searchChange}
       />
+      <label for='searchRobots'>Search Robots</label>
     </div>
   );
 }
